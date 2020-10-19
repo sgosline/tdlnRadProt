de1324b3b84a45788d1795043051755ce008bdd9
Sara Gosline <sara.gosline@pnnl.gov>
2020-10-18 20:27:41 -0700

    Create mapProtsToRad.py
    going to create a file to do the mapping of the two data types

---
+++ data/mapProtsToRad.py
@@ -0,0 +1,18 @@
+'''
+Downloads radiomic data
+maps patient identifiers to PDC
+gets protein data
+'''
+
+
+from osfclient import cli
+
+
+def main():
+    '''
+    download the files
+    get the file mapping
+    '''
+
+if __name__=='__main__':
+    main()
