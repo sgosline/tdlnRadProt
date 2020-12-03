14daa24c4f8ec14ad05b8935c059ab90eab9ba05
Sara Gosline <sara.gosline@pnnl.gov>
2020-12-02 19:48:02 -0800

    worked toward the osf client

--- data/mapProtsToRad.py
+++ data/mapProtsToRad.py
@@ -7,6 +7,9 @@ gets protein data
 
 from osfclient import cli
 
+token = "C6a7xYr87esl61NiQaXOHCYZykMVwiE0kLscQkD62fXmfMVgj1A1fFgJQkDG7ZuyNWeEc5"
+image_dat = '5x4n2/NewImageData02142017.xlsx'
+prot_dat = '5x4n2/S054_HNSCC_imputed_0920.tsv'
 
 def main():
     '''
